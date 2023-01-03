86d22a081c800e5da990ca70b60f8bfb36963ec2
bvanderhorn <bvanderhorn@gmail.com>
2023-01-03 17:28:20 +0100

    chore: cleanup

--- src/1/day1.ts
+++ src/1/day1.ts
@@ -4,7 +4,7 @@ import * as h from "../helpers";
 var inputFile = 'sweep.txt';
 
 // parse
-var input = h.read(1,inputFile,'toint');
+var input = h.read(1,inputFile,'toint|ex');
 h.print(input.slice(0,10));
 var incr = input.slice(1).map((el,i) => (el > input[i]) ? "1" : "0").reduce((a,b) => +a + +b, 0);
 h.print(incr);
