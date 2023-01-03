ee5de62a3f4edb36ae573343e0dbb1787d193f29
bvanderhorn <bvanderhorn@gmail.com>
2023-01-03 18:18:16 +0100

    feat: day1 part 2

--- src/1/day1.ts
+++ src/1/day1.ts
@@ -4,7 +4,14 @@ import * as h from "../helpers";
 var inputFile = 'sweep.txt';
 
 // parse
-var input = h.read(1,inputFile,'toint|ex');
+var input = h.read(1,inputFile,'toint');
 h.print(input.slice(0,10));
+
+// part 1
 var incr = input.slice(1).map((el,i) => (el > input[i]) ? "1" : "0").reduce((a,b) => +a + +b, 0);
 h.print(incr);
+
+// part 2
+var sumList = input.slice(2).map((el, i) => input[i] + input[i+1] + el);
+var incrSum = sumList.slice(1).map((el, i) => (el > sumList[i]) ? "1" : "0").reduce((a, b) => +a + +b, 0);
+h.print(incrSum);
