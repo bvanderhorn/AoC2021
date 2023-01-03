75387b908ec9e2029cdf1ab51906043a4c8e6f30
bvanderhorn <bvanderhorn@gmail.com>
2023-01-03 23:07:24 +0100

    chore: refactor

--- src/2/day2.ts
+++ src/2/day2.ts
@@ -1,11 +1,7 @@
 import * as h from "../helpers";
 
 // functions
-var inDir = (input:string[], direction:string) : number  => {
-    var sub = input.filter(el => el.includes(direction));
-    sub.push(' 0');
-    return sub.map(el => +el.split(' ')[1]).sum();
-} 
+var inDir = (input:string[], direction:string) : number  => [...input.filter(el => el.includes(direction)), ' 0'].map(el => +el.split(' ')[1]).sum();
 
 // parse
 var input = h.read(2,'instructions.txt');
@@ -13,10 +9,10 @@ var input = h.read(2,'instructions.txt');
 // part 1
 var xy: number[] = [inDir(input,'forward'), inDir(input,'down') - inDir(input,'up')];
 h.print('final position: ', xy);
-h.print('multiplied: ', xy.multiply());
+h.print(' multiplied: ', xy.multiply());
 
 // part 2
 var m = input.map((el,i) => (inDir(input.slice(0,i+1),'down') - inDir(input.slice(0,i+1),'up'))*(+el.split(' ')[1]) );
 xy = [inDir(input,'forward'), m.map((el,i) => input[i].includes('forward') ? el : 0).sum()];
-h.print(' new final position: ',xy)
-h.print('multiplied: ', xy.multiply());
+h.print('new final position: ',xy)
+h.print(' multiplied: ', xy.multiply());
