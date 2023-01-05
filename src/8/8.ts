02f84068c2288b3ac3fe75727a2503f31bc0e63b
bvanderhorn <bvanderhorn@gmail.com>
2023-01-05 16:47:45 +0100

    feat: day 8 cleanup

--- src/8/8.ts
+++ src/8/8.ts
@@ -2,22 +2,12 @@ import * as h from "../helpers";
 var dgot = (display:string[][], contains: number) : boolean => display.flat().map(s => s.length == contains ? 1 : 0).sum() > 0;
 var get14 = (display:string[][]) : string[] => [display.flat().filter(d => d.length === 2)[0], display.flat().filter(d => d.length === 4)[0]];
 var translate = (str:string, ex1:string, ex4: string) : string => {
-    if (str.length == 2) return "1";
-    if (str.length == 4) return "4";
-    if (str.length == 3) return "7";
-    if (str.length == 7) return "8";
-    var sharedWith1 = h.range(0,ex1.length).map(i => str.includes(ex1[i]) ? 1 : 0).sum();
-    var sharedWith4 = h.range(0,ex4.length).map(i => str.includes(ex4[i]) ? 1 : 0).sum();
-    if (str.length == 6) {
-        if (sharedWith1 == 1) return "6"; 
-        if (sharedWith4 == 4) return "9"; 
-        return "0";
-    } 
-    if (str.length == 5) {
-        if (sharedWith1 == 2) return "3"; 
-        if (sharedWith4 == 2) return "2"; 
-    }
-    return "5";
+    var sl = str.length;
+    var sw1 = h.range(0,ex1.length).map(i => str.includes(ex1[i]) ? 1 : 0).sum();
+    var sw4 = h.range(0,ex4.length).map(i => str.includes(ex4[i]) ? 1 : 0).sum();
+    var numbers = [1,4,7,8,6,9,0,3,2,5];
+    var checks = [sl==2, sl==4, sl==3, sl==7, sl==6 && sw1==1, sl==6 && sw4==4, sl==6, sl==5 && sw1==2, sl==5 && sw4==2, true];
+    return "" + numbers[checks.indexOf(true)];
 }
 var translateDisplay = (display:string[][]) : string[][] => display.map(l => l.map(str => translate(str,get14(display)[0],get14(display)[1])));
 var displays = h.read(8,'displays.txt').map(d => d.split(/\s+\|\s+/).map((l:string)=> l.split(' ')));
