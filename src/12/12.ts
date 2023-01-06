f218af5e17ddea07b77b5a9c908605529976e0ac
bvanderhorn <bvanderhorn@gmail.com>
2023-01-06 15:49:02 +0100

    feat: day 12 caves list

--- src/12/12.ts
+++ src/12/12.ts
@@ -1,3 +1,5 @@
 import * as h from "../helpers";
-var caves = h.read(12,'caves.txt').split('-');
-h.print(caves);
+var links = h.read(12,'caves.txt').split('-');
+var caves = links.flat().unique().map(c => [c, links.filter(l => l.includes(c)).flat().filter(l => l != c).sort()]);
+h.print(links);
+h.print(caves.sort((a,b) => a[1].length > b[1].length ? -1 : 1)); 
