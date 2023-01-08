0d4704204131736bb4cf42d71417301e1529f561
bvanderhorn <bvanderhorn@gmail.com>
2023-01-09 00:20:34 +0100

    chore: cleaner

--- src/16/16.ts
+++ src/16/16.ts
@@ -1,4 +1,4 @@
 import * as h from "../helpers";
-var hex2bin = (hex:string): string => (parseInt(hex, 16).toString(2));
+var hex2bin = (hex:string): string => parseInt(hex, 16).toString(2);
 var packet = h.read(16,'packet.txt');
 h.print(hex2bin('D2FE28'));
