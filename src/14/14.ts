990a152833f3f64e9a6af08e641c617e7e3a833e
bvanderhorn <bvanderhorn@gmail.com>
2023-01-08 01:14:08 +0100

    feat: day 14 part 1

--- src/14/14.ts
+++ src/14/14.ts
@@ -1,17 +1,26 @@
 import * as h from "../helpers";
-var step = (poly:string[], ins:string[][]) : string[] => {
-    return h.eArray(poly.length*2+1).map((_,i) => i%2==0 ? poly[i/2] : _)
+var step = (buckets:number[], ins:string[][]) : number[] => {
+  var newb:number[] = h.eArray(buckets.length, 0);
+  for (const i of buckets.range()) {
+    let [pair,to] = ins[i];
+    newb[ins.col(0).indexOf(pair[0]+to)] += buckets[i];
+    newb[ins.col(0).indexOf(to+pair[1])] += buckets[i];
+  }
+  return newb;
 }
+var btol = (buckets:number[],letters:string[],pairs:string[],poly:string): number[] => letters.map(l => pairs.map((p,i)=> p.split('').count(l)*buckets[i]).sum() + [poly[0],poly.slice(-1)].count(l)).times(1/2);
 var [poly, ins] = h.read(14, 'polymers1.txt');
 ins = ins.split(' -> ').sort((a:string[],b:string[])=> a[0]<b[0] ? -1 : 1).filter((i:any[]) => i[0] != '');
 poly = poly[0];
-var pairs = ins.col(0).sort();
-var letters = pairs.join('').split('').unique();
+const pairs = ins.col(0);
+const letters = pairs.join('').split('').unique();
 var ibuckets: number[] = h.eArray(pairs.length,0);
 for (let i=1;i<poly.length;i++) ibuckets[pairs.indexOf(poly.slice(i-1,i+1))]++;
+var buckets = ibuckets.copy();
+for (const i of h.range(0,10)) buckets = step(buckets,ins);
+var fletters = btol(buckets,letters,pairs,poly);
 h.print(poly);
-h.print(ins);
-h.print(pairs.length);
 h.print(letters);
-h.print(pairs);
-h.print(ibuckets);
+h.print();
+//h.print(pairs.map((p:string,i:number)=> p+ibuckets[i]).filter((p:string) => !p.includes('0')));
+h.print('part 1: most common ',fletters.max(),' minus least common ',fletters.min(),' = ',fletters.max() - fletters.min());
