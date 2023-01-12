682b4c813613c268a56a67d6b33d7fe3cb0db834
bvanderhorn <bvanderhorn@gmail.com>
2023-01-12 23:56:42 +0100

    feat: applytransform, checktransform

--- src/19/19.ts
+++ src/19/19.ts
@@ -10,12 +10,26 @@ var pointpairs = (scanners:number[][][], pointmatches: [number[], number[][]][],
     let pointmatch : [number[], number[][]] = pointmatches[pointmatches.findIndex(pm => h.equals2(pm[0],[s1, s2]))]
     return pointmatch[1].map(pp => [scanners[s1][pp[0]], scanners[s2][pp[1]]]);
 }
+var flips8: string[] = ['+++','++-','+-+','+--','-++','-+-','--+','--'];
+var permutations6 : number[][] = [[0,1,2],[0,2,1],[1,0,2],[1,2,0],[2,0,1],[2,1,0]];
+var rotations48 : [string, number[]][] = flips8.map(f => permutations6.map(p => [f, p])).flat();
+var applyRotation = (point:number[], rotation: [string, number[]]) : number[] => {
+    let pnew : number[] = [];
+    for (const i of point.range()) pnew.push((rotation[0][i] === '-' ?-1:1)*point[rotation[1][i]]);
+    return pnew;
+}
+var applyTranslation = (point:number[], translation:number[]) : number[] => [point,translation].sum0();
 var getTransform = (b1: number[], b2:number[], rotation: [string, number[]]) : [[string, number[]],number[]] => {
     // find transform to map b2 to b1 when applying given rotation to b2 coordinates
-    let b2new : number[] = [];
-    for (const i of b2.range()) b2new.push((rotation[0][i] === '-' ?-1:1)*b2[rotation[1][i]]);
+    let b2new = applyRotation(b2, rotation);
     let translation : number[] = [b1[0]-b2new[0], b1[1]-b2new[1], b1[2]-b2new[2]];
     return [rotation, translation];
+}
+var getAllTransforms = (b1:number[], b2: number[]) : [[string, number[]],number[]][] => rotations48.map(r => getTransform(b1,b2,r));
+var applyTransform = (point:number[], transform:[[string, number[]],number[]]) : number[] => applyTranslation(applyRotation(point, transform[0]),transform[1]);
+var checkTransform = (b1:number[], b2:number[], transform:[[string, number[]],number[]]) : boolean => {
+    // check if applying transform to b2 gives b1
+    return h.equals2(b1, applyTransform(b2, transform));
 }
 
 console.time("day 19");
@@ -26,17 +40,24 @@ var matching: number[][] = dcs.map((dc,i)=> matchingscanners(dc,i));
 var pointmatches: [number[], number[][]][] = matching.map((l,i)=> l.map(m => [[i,m],pointmatch(dcs[i][m])])).flat().filter(pm => pm[0] < pm[1]);
 
 // part 1 try 2: do actual mapping of all points
-var flips8: string[] = ['+++','++-','+-+','+--','-++','-+-','--+','--'];
-var permutations6 : number[][] = [[0,1,2],[0,2,1],[1,0,2],[1,2,0],[2,0,1],[2,1,0]];
-var rotations48 : [string, number[]][] = flips8.map(f => permutations6.map(p => [f, p])).flat();
-h.print(rotations48.slice(0,3));
-h.print(getTransform([1,2,3], [4,5,6],['+-+',[0,2,1]]));
+var pp09 = pointpairs(scanners,pointmatches,0,9);
+var transforms09 = getAllTransforms(pp09[0][0],pp09[0][1]);
+h.write(19,'transforms09.json',h.stringify(transforms09));
+var tcheck = transforms09.map(t => checkTransform(pp09[0][0],pp09[0][1],t));
+h.print(tcheck);
+
+// h.print(rotations48.slice(0,3));
+var rot: [string, number[]] = ['+--',[2,0,1]];
+var tr = getTransform([1,2,3], [4,5,6],rot);
+h.print(tr);
+h.print(applyTransform([4,5,6],tr));
+// h.print()
 
 // showing some stuff
 // h.print(dcs[0][9].printcolor(x => x>=12,'r',','));
-h.print(pointmatch(dcs[0][9]));
+// h.print(pointmatch(dcs[0][9]));
 h.print(matching);
-h.print(pointpairs(scanners,pointmatches,0,9));
+h.print(pp09);
 h.write(19,'pointmatches.json',h.stringify(pointmatches));
 
 console.timeEnd("day 19");
