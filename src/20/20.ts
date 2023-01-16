48e976747a1265580687fb13fbb0806132b505ef
bvanderhorn <bvanderhorn@gmail.com>
2023-01-16 20:29:51 +0100

    feat: day 20 init

---
+++ src/20/20.ts
@@ -0,0 +1,6 @@
+import * as h from "../helpers";
+let [alg, floor] = h.read(20,'floor.txt').split('');
+alg = alg[0].map((a:string) => a==='#' ?1:0);
+floor = floor.mape((x:string) => x==='#' ?1:0);
+h.print(alg);
+h.print(floor.print());
