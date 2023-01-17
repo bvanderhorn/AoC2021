e47e82bf1ab00096aa969903544967c6d1ef427c
bvanderhorn <bvanderhorn@gmail.com>
2023-01-18 00:41:48 +0100

    feat: day 20 part 1

--- src/20/20.ts
+++ src/20/20.ts
@@ -1,13 +1,18 @@
 import * as h from "../helpers";
-var addZeros = (floor:number[][]) : number[][] => {
+var addLayer = (floor:number[][],x:number) : number[][] => {
     let dims = floor.dims();
-    return [h.ea(dims[1]+2,0), ...floor.map(f => [0, ...f, 0]), h.ea(dims[1]+2,0)];
+    return [h.ea(dims[1]+2,x), ...floor.map(f => [x, ...f, x]), h.ea(dims[1]+2,x)];
 }
-var enhance = (floor:number[][], alg:number[]) : number[][] => {
-    let dims = floor.dims();
-    let out = floor.mapij((i,j,_) => {
+var enhance = (floor:number[][], alg:number[],index:number) : number[][] => {
+    let x:number;
+    if (alg[0]===0) x = 0;
+    else if (alg.slice(-1)[0] ===0) x = index % 2;
+    else x = 1;
+    let out = addLayer(floor,x);
+    let dims = out.dims();
+    out = out.mapij((i,j,_) => {
         let nb:number[][] = h.getnb([i,j],0,0,'9nf').sort((a,b) => a[0] - b[0] || a[1] - b[1]);
-        let bin:string = nb.map((n:number[]) => (n.min() < 0 || n[0] >= dims[0] || n[1] >= dims[1]) ? 0: floor[n[0]][n[1]]).join('');
+        let bin:string = nb.map((n:number[]) => (n.min() < 0 || n[0] >= dims[0] || n[1] >= dims[1]) ? x: out[n[0]][n[1]]).join('');
         return alg[parseInt(bin,2)];
     });
     out.mape(x=>x==1?'#':'.').printc(x=>x=='#','r');
@@ -16,11 +21,11 @@ var enhance = (floor:number[][], alg:number[]) : number[][] => {
 }
 var enhanceTimes = (floor:number[][], alg:number[], times:number): number[][] => {
     let out = floor.copy();
-    for(let i=0;i<times;i++) out = enhance(addZeros(out),alg);
+    for(let i=0;i<times;i++) out = enhance(out,alg,i);
     return out;
 }
 
-let [alg, floor] = h.read(20,'floor.txt','ex').split('');
+let [alg, floor] = h.read(20,'floor.txt').split('');
 alg = alg[0].map((a:string) => a==='#' ?1:0);
 floor = floor.mape((x:string) => x==='#' ?1:0);
 floor.mape(x=>x==1 ? '#':'.').printc(x=>x=='#','c');
