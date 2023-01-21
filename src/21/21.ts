97c393a59d6480716e658a8540b6c38273b76b7b
bvanderhorn <bvanderhorn@gmail.com>
2023-01-21 01:50:36 +0100

    feat: day 21 even more progress

--- src/21/21.ts
+++ src/21/21.ts
@@ -38,16 +38,16 @@ var getPaths21 = (start:number) : number[][] => {
 var pathsNotTo21 = (path:number[],paths:number[][], index:number[],turn:number) : void => {
   for (const x of h.range(3,10)) {
     let nextPath = path.concat([mod(path.last()+x)]);
-    if (nextPath.slice(1).sum() < 21) {
-      if (nextPath.length === turn+1) {
-        paths[index[0]] = nextPath;
-        index[0]++;
-        //h.progress(index[0],1e7,1000);
+    if (nextPath.length === turn+1) {
+      if (nextPath.slice(1).sum() < 21) {
+          paths[index[0]] = nextPath;
+          index[0]++;
+          //h.progress(index[0],1e7,1000);
       }
-      else  if (nextPath.length < turn+1) {
-        pathsTo21(nextPath, paths,index);
-      } 
     }
+    else  if ((nextPath.length < (turn+1)) && (nextPath.slice(1).sum() < 21)) {
+      pathsNotTo21(nextPath, paths,index,turn);
+    } 
   }
 }
 var getPathsNot21 = (start:number, turn:number) : number[][] => {
@@ -58,7 +58,8 @@ var getPathsNot21 = (start:number, turn:number) : number[][] => {
 }
 // var uniquePathsUpToTurnNot21 = (paths:number[][],turn:number):number[][] => paths.filter(p=>p.length > turn+1).map(p=>p.slice(0,turn+2)).unique();
 // var uptCount = (paths:number[][],maxTurns:number) : number[] => h.range(0,maxTurns+1).map(t => uniquePathsUpToTurnNot21(paths,t).length);
-var uptCount = (start:number,maxTurns:number) : number[] => h.range(0,maxTurns+1).map(t => getPathsNot21(start,t).length);
+var pathsNot21PerTurn = (start:number,maxTurns:number) : number[][][] => h.range(0,maxTurns+1).map(t => getPathsNot21(start,t));
+var countNot21PerTurn = (start:number, maxTurns:number) : number[] => pathsNot21PerTurn(start, maxTurns).map(t => t.map(p => pathCount(p)).sum());
 var dCount = [0,0,0,1,3,6,7,6,3,1,0];
 var deltas = (path:number[]) : number[] => path.slice(1).map((p,i) => mod(p+10-path[i]));
 var pathCount = (path:number[]) : number => deltas(path).map(d => dCount[d]).prod();
@@ -67,20 +68,22 @@ var turnsCountArray = (paths:number[][], maxTurns:number) : number[] => h.range(
 let [pathsA, pathsB] = start.map(s => getPaths21(s));
 let maxTurns = pathsA.map(p => p.length).concat(pathsB.map(p=>p.length)).max()-1;
 let [tcA,tcB] = [pathsA,pathsB].map(p => turnsCountArray(p,maxTurns));
-let [uptA, uptB] = start.map(s => uptCount(s,maxTurns));
+let [uptA, uptB] = start.map(s => pathsNot21PerTurn(s,maxTurns).map(p=>p.length));
+let [ncA, ncB] = start.map(s => countNot21PerTurn(s,maxTurns));
 // let Awins = tcA.map((tc,i) => tcB.slice(0,i).sum()*tc).sum();
 // let Bwins = tcB.map((tc,i) => tcA.slice(0,i).sum()*tc).sum();
 
 // h.print(deltas([5,1,4,1,4,2,1,4,2,1,4]));
 // h.print(pathCount([5,1,4,1,4,2,1,4,2,1,4]));
+h.print(getPathsNot21(5,2));
 
 h.print('nof paths for ',start[0],': ',pathsA.length);
 h.print('nof paths for ',start[1],': ',pathsB.length);
 h.print('max Turns: ',maxTurns);
-h.print('unique paths to 21 in turns for A: ',tcA);
-h.print('unique paths to 21 in turns for B: ',tcB);
-h.print('unique paths not 21 up to turns for A: ',uptA);
-h.print('unique paths not 21 up to turns for B: ',uptB);
+h.print('unique variants to 21 in turns for A: ',tcA);
+h.print('unique variants to 21 in turns for B: ',tcB);
+h.print('unique variants not 21 up to turns for A: ',ncA);
+h.print('unique variants not 21 up to turns for B: ',ncB);
 // h.print('player A wins in: ',Awins);
 // h.print('player B wins in: ',Bwins);
 // pathsA.sort((a,b) => b.length - a.length).slice(0,30).print(',');
