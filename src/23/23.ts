fed2a9d7aa26a283d8def85356de54b5aaa7b374
Bram <bvanderhorn@gmail.com>
2023-04-15 16:39:02 +0200

    feat: more day 23

--- src/23/23.ts
+++ src/23/23.ts
@@ -3,13 +3,16 @@ var types = 'ABCD';
 var input = h.read(23,'amphipods.txt').mape(l => l.replace(/[\W]/g,'').replace(/\w/g, (m:string) => types.indexOf(m))).filter(l => l).reverse().split('').transpose().tonum();
 h.print(input);
 var pods = h.range(0, input.length);
+var depth = input[0].length;
 
 var alley : number[] = h.ea(11);
 
 h.print(alley);
 
 // full search
-var entryPoint = (i: number) => 2 + 2*i;
+var entryPoint = (i: number) : number => 2 + 2*i;
+var isEmptyOrContainsOnly = (i: number, j: number) : boolean => input[i].every((x: number) => x == j);
+var canSet = (pod: number) : boolean => isEmptyOrContainsOnly(pod, pod);
 var entryPoints = pods.map(r => entryPoint(r));
 var rests : number[] = h.range(0,alley.length).filter(i => !entryPoints.includes(i));
 
