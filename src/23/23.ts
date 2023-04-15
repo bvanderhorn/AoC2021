2591835ccaf05a28b6fe77d12e5a9b52b86688d2
Bram <bvanderhorn@gmail.com>
2023-04-15 16:31:47 +0200

    more day 23

--- src/23/23.ts
+++ src/23/23.ts
@@ -2,6 +2,7 @@ import * as h from '../helpers';
 var types = 'ABCD';
 var input = h.read(23,'amphipods.txt').mape(l => l.replace(/[\W]/g,'').replace(/\w/g, (m:string) => types.indexOf(m))).filter(l => l).reverse().split('').transpose().tonum();
 h.print(input);
+var pods = h.range(0, input.length);
 
 var alley : number[] = h.ea(11);
 
@@ -9,10 +10,14 @@ h.print(alley);
 
 // full search
 var entryPoint = (i: number) => 2 + 2*i;
-var entryPoints = h.range(0, input.length).map(r => entryPoint(r));
+var entryPoints = pods.map(r => entryPoint(r));
 var rests : number[] = h.range(0,alley.length).filter(i => !entryPoints.includes(i));
 
 h.print(input[0].last());
 h.print(entryPoints);
 h.print(rests);
 
+var multiplier = pods.map(i => Math.pow(10,i));
+var stepsTaken = h.ea(pods.length, 0);
+h.print(stepsTaken);
+h.print(multiplier);
