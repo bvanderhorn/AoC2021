a578b611212d2089bf15be56f1b80ede7e14ad55
Bram <bvanderhorn@gmail.com>
2023-04-15 16:18:08 +0200

    feat: more day 23

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 Advent of Code 2021. Mainly used to create helper functions for upcoming AoC's.
 
 ## How to set up the TypeScript project
-1. In main repo folder, run `npm install --save-dev typescript @types/node fs`
-1. copy `tsconfig.json`, `.gitignore`
-1. `Ctrl` + `Shift` + `B` -> 'watch'
+1. In main repo folder, run:
+
+```shell
+npm install --save-dev typescript @types/node fs
+```
+
+2. copy `tsconfig.json`, `.gitignore`
+3. `Ctrl` + `Shift` + `B` -> 'watch'
 
 ## How to create and run the script for a given day
 1. Place `helper.ts` in `src` folder
--- src/23/23.ts
+++ src/23/23.ts
@@ -3,3 +3,16 @@ var types = 'ABCD';
 var input = h.read(23,'amphipods.txt').mape(l => l.replace(/[\W]/g,'').replace(/\w/g, (m:string) => types.indexOf(m))).filter(l => l).reverse().split('').transpose().tonum();
 h.print(input);
 
+var alley : number[] = h.ea(11);
+
+h.print(alley);
+
+// full search
+var entryPoint = (i: number) => 2 + 2*i;
+var entryPoints = h.range(0, input.length).map(r => entryPoint(r));
+var rests : number[] = h.range(0,alley.length).filter(i => !entryPoints.includes(i));
+
+h.print(input[0].last());
+h.print(entryPoints);
+h.print(rests);
+
