6b6f814200bb0e8b2852a5a90f0b567eca1bce71
Bram <bvanderhorn@gmail.com>
2023-04-15 16:54:16 +0200

    feat: more day 23

--- src/23/23.ts
+++ src/23/23.ts
@@ -10,10 +10,21 @@ var alley : number[] = h.ea(11);
 h.print(alley);
 
 // full search
-var entryPoint = (i: number) : number => 2 + 2*i;
+var getEntryPoint = (i: number) : number => 2 + 2*i;
 var isEmptyOrContainsOnly = (i: number, j: number) : boolean => input[i].every((x: number) => x == j);
 var canSet = (pod: number) : boolean => isEmptyOrContainsOnly(pod, pod);
-var entryPoints = pods.map(r => entryPoint(r));
+var isReachable = (alley: number[], entryPoint: number, rest: number) : boolean => {
+    var subAlley = alley.slice(Math.min(entryPoint, rest), Math.max(entryPoint, rest) + 1);
+    return subAlley.every(x => x == undefined || x == null);
+}
+var restOptions = (entryPoint:number, alley: number[], rests: number[]) : number[] => {
+    var options : number[] = [];
+    for (const rest of rests) {
+        if (isReachable(alley, entryPoint, rest)) options.push(rest);
+    }
+    return options;
+}
+var entryPoints = pods.map(r => getEntryPoint(r));
 var rests : number[] = h.range(0,alley.length).filter(i => !entryPoints.includes(i));
 
 h.print(input[0].last());
@@ -24,3 +35,6 @@ var multiplier = pods.map(i => Math.pow(10,i));
 var stepsTaken = h.ea(pods.length, 0);
 h.print(stepsTaken);
 h.print(multiplier);
+h.print(multiplier.slice(1,3));
+
+
