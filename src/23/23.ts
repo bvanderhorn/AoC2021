7b4da6ba58a6944d06c827886cc4c545ded02489
Bram <bvanderhorn@gmail.com>
2023-04-17 21:09:46 +0200

    feat: progress

--- src/23/23.ts
+++ src/23/23.ts
@@ -1,22 +1,4 @@
 import * as h from '../helpers';
-var types = 'ABCD';
-var input = h.read(23,'amphipods.txt').mape(l => l.replace(/[\W]/g,'').replace(/\w/g, (m:string) => types.indexOf(m))).filter(l => l).reverse().split('').transpose().tonum();
-h.print(input);
-var pods = h.range(0, input.length);
-var depth = input[0].length;
-
-var alley : number[] = h.ea(11);
-var entryPoints = pods.map(r => getEntryPoint(r));
-var rests : number[] = h.range(0,alley.length).filter(i => !entryPoints.includes(i));
-
-type state = {
-    burrows: number[][],
-    alley: number[],
-    points: number
-}
-
-h.print(alley);
-
 // helpers
 var getEntryPoint = (i: number) : number => 2 + 2*i;
 var isEmptyOrContainsOnly = (i: number, j: number, burrows: number[][]) : boolean => burrows[i].every((x: number) => x == j);
@@ -25,8 +7,16 @@ var stepsOut = (lengthBefore:number) : number => depth - lengthBefore + 1;
 var stepsIn = (lengthBefore: number) : number => depth - lengthBefore;
 var isReachable = (alley: number[], from: number, to: number) : boolean => {
     var subAlley = alley.slice(Math.min(from, to) + 1, Math.max(from, to) + 1);
-    return subAlley.every(x => x == undefined || x == null);
+    return subAlley.every(x => x == -1);
 }
+
+var copyState = (state: state) : state => {
+    return {
+        burrows: state.burrows.copy(),
+        alley: state.alley.copy(),
+        points: state.points
+    }
+};
 var getRestOptions = (entryPoint:number, alley: number[], rests: number[]) : number[] => {
     var options : number[] = [];
     for (const rest of rests) {
@@ -37,7 +27,7 @@ var getRestOptions = (entryPoint:number, alley: number[], rests: number[]) : num
 var getDirectMove = (state: state) : state | undefined => {
     // direct move from alley
     for (var i = 0; i < state.alley.length; i++) {
-        if (state.alley[i] == undefined) continue;
+        if (state.alley[i] == -1) continue;
 
         var pod = state.alley[i];
         var entryPoint = i;
@@ -45,8 +35,8 @@ var getDirectMove = (state: state) : state | undefined => {
         
         if (isReachable(state.alley, entryPoint, homePoint) && canSet(pod, state.burrows)) {
             var addPoints = stepsIn(state.burrows[pod].length) + Math.abs(homePoint - entryPoint);
-            var newState = JSON.parse(JSON.stringify(state));
-            newState.alley[i] = undefined;
+            var newState = copyState(state);
+            newState.alley[i] = -1;
             newState.burrows[pod].push(pod);
             newState.points += addPoints;
             return newState;
@@ -64,18 +54,18 @@ var getDirectMove = (state: state) : state | undefined => {
         
         if (isReachable(state.alley, entryPoint, homePoint) && canSet(pod, state.burrows)) {
             var addPoints = stepsOut(state.burrows[i].length) + stepsIn(state.burrows[pod].length) + Math.abs(homePoint - entryPoint);
-            var newState = JSON.parse(JSON.stringify(state));
-            newState.burrows[i].pop();
-            newState.burrows[pod].push(pod);
-            newState.points += addPoints;
-            return newState;
+            var newState2 = copyState(state);
+            newState2.burrows[i].pop();
+            newState2.burrows[pod].push(pod);
+            newState2.points += addPoints;
+            return newState2;
         }
     }
 
     return undefined;
 }
 
-var getMoveOptions = (state: state) : state[] => {
+var getNextStates = (state: state) : state[] => {
     // return direct move if exists
     var directMove = getDirectMove(state);
     if (directMove != undefined) return [directMove];
@@ -92,7 +82,7 @@ var getMoveOptions = (state: state) : state[] => {
         var restOptions = getRestOptions(entryPoint, state.alley, rests);
         for (const rest of restOptions) {
             var addPoints = stepsOut(burrow.length) + Math.abs(rest - entryPoint);
-            var newState = JSON.parse(JSON.stringify(state));
+            var newState = copyState(state);
             newState.burrows[i].pop();
             newState.alley[rest] = pod;
             newState.points += addPoints;
@@ -113,18 +103,55 @@ var removeDuplicates = (states: state[]) : state[] => {
     return result;
 }
 
-var sortStates = (states: state[]) : state[] => states.sort((a,b) => a.points - b.points);
+var sortStates = (states: state[]) : void => {
+    states.sort((a,b) => a.points - b.points);
+}
 var stateIsFinal = (state: state) : boolean => state.burrows.every((_, i) => canSet(i, state.burrows)) && state.alley.every(x => x == undefined);
 
-h.print(input[0].last());
-h.print(entryPoints);
-h.print(rests);
+// init
+var types = 'ABCD';
+var input = h.read(23,'amphipods.txt').mape(l => l.replace(/[\W]/g,'').replace(/\w/g, (m:string) => types.indexOf(m))).filter(l => l).reverse().split('').transpose().tonum();
+h.print(input);
+var pods = h.range(0, input.length);
+var depth = input[0].length;
 
-var multiplier = pods.map(i => Math.pow(10,i));
-var stepsTaken = h.ea(pods.length, 0);
-h.print(stepsTaken);
-h.print(multiplier);
-h.print(multiplier.slice(1,3));
+var alley : number[] = h.ea(11,-1);
+var entryPoints = pods.map(r => getEntryPoint(r));
+var rests : number[] = h.range(0,alley.length).filter(i => !entryPoints.includes(i));
+
+type state = {
+    burrows: number[][],
+    alley: number[],
+    points: number
+}
 
 // Dijkstra
-
+var startState : state = {
+    burrows: input,
+    alley: alley,
+    points: 0
+}
+var statesToCheck: state[] = [startState];
+var lowestEndPoints = 1E8;
+var loopCounter = 0;
+while (statesToCheck.length > 0) {
+    if (loopCounter % 50 == 0) h.print(`loop ${loopCounter}: ${statesToCheck.length} states to check`);
+    var currentState = statesToCheck.shift();
+    var nextStates = getNextStates(currentState!);
+    while (nextStates.length > 0) {
+        var nextState = nextStates.pop();
+        if (stateIsFinal(nextState!)) {
+            if (nextState!.points < lowestEndPoints) {
+                lowestEndPoints = nextState!.points;
+                h.print(`new lowest state on loop ${loopCounter}: ${nextState!.points}`);
+            }
+            lowestEndPoints = Math.min(lowestEndPoints, nextState!.points);
+            continue;
+        }
+        statesToCheck.push(nextState!);
+    }
+    statesToCheck = statesToCheck.filter(s => s.points < lowestEndPoints);
+    statesToCheck = removeDuplicates(statesToCheck);
+    sortStates(statesToCheck);
+    loopCounter++;
+}
