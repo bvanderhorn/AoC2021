ba7657b5a8368fb3e57e4f6489c91835544fbc35
bvanderhorn <bvanderhorn@gmail.com>
2023-04-20 14:30:55 +0200

    feat: start instruction interpreter

--- src/24/24.ts
+++ src/24/24.ts
@@ -1,6 +1,14 @@
 import * as h from '../helpers';
 // helpers
+var execute = (instruction: string[], state: number[], input: number) : void {
+    let [w, x ,y, z] = state;
+    
+    switch (instruction[0]) {
+        case 'inp' : eval(instruction[1]+ " = " + input); break;
+    }
 
+    state = [w, x, y, z];
+}
 
 var instructions = h.simpleRead(24,'monad.txt').split(/inp/).filter(x => x!= "").map(x => "inp" + x).split(/\r?\n/).map(x => x.filter(l => l != '')).split(" ");
 h.print(instructions[0]);
