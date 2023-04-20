415b4a2873a4c3422ebac9b07803d1a6c081d4e3
bvanderhorn <bvanderhorn@gmail.com>
2023-04-20 16:58:49 +0200

    feat: progress

--- src/24/24.ts
+++ src/24/24.ts
@@ -9,7 +9,7 @@ var executeInstruction = (instruction: string[], state: number[], input: number)
     if (instruction[0] == 'inp') executionString = `${instruction[1]} = ${input}`;
     else executionString = `${instruction[1]} = +(${instruction[1]} ${operator[command.findIndex(x => x==instruction[0])]} ${instruction[2]})`;
     
-    h.print(" executiong: " + executionString);
+    // h.print(" executiong: " + executionString);
     eval(executionString);
 
     state[0] = w;
@@ -17,14 +17,22 @@ var executeInstruction = (instruction: string[], state: number[], input: number)
     state[2] = y;
     state[3] = z;
 }
-var executeInstructionSet = (instructions: string[][], state: number[], input: number) : void => {
+var executeInstructionSet = (instructions: string[][], input: number, state: number[] = [0,0,0,0]) : number[] => {
     instructions.forEach(x => executeInstruction(x, state, input));
+    return state;
+}
+var executeMonad = (instructionSets: string[][][], input: string) : number => {
+    var result = "";
+    instructionSets.forEach((x,i) => {
+        result += executeInstructionSet(x, +input[i])[3];
+    });
+    return +result;
 }
 
 var instructionSets = h.simpleRead(24,'monad.txt').split(/inp/).filter(x => x!= "").map(x => "inp" + x).split(/\r?\n/).map(x => x.filter(l => l != '')).split(" ");
 h.print(instructionSets[0]);
 
-var state = [0,0,0,0];
+// var state = [0,0,0,0];
 // executeInstruction(["add", "w", "1"], state, 0);
-executeInstructionSet(instructionSets[0], state, 1);
-h.print(state);
+h.print(executeInstructionSet(instructionSets[0], 1));
+h.print(executeMonad(instructionSets, "13579246899999"));
